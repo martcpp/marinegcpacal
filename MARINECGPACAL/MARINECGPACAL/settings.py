7282ecee5fec0f9c14e667feb11ece810a1f2162
martcpp <mordecaiemmanueletukudo@gmail.com>
2023-05-06 01:46:13 +0100

    vercel

--- MARINECGPACAL/MARINECGPACAL/settings.py
+++ MARINECGPACAL/MARINECGPACAL/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-a+qkf96=4u8%_$z=6*9mrhj7&=ojq^#qlf(r#ft(*ejnf1*bgy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh']
+ALLOWED_HOSTS = [ ]
 
 
 # Application definition
@@ -76,10 +76,10 @@ WSGI_APPLICATION = 'MARINECGPACAL.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-     #   'ENGINE': 'django.db.backends.sqlite3',
-       # 'NAME': BASE_DIR / 'db.sqlite3',
-   # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
@@ -117,8 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+
 
 
 # Default primary key field type
